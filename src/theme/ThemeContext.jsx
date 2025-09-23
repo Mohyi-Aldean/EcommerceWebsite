298903faec7f55e0814d992422ac6839712b1f68
Hasoon <144700795+Mohyi-Aldean@users.noreply.github.com>
2025-09-23 16:24:23 +0300

    Refactor ThemeContext to persist theme mode in localStorage and improve theme toggling logic

--- src/theme/ThemeContext.jsx
+++ src/theme/ThemeContext.jsx
@@ -1,16 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
 export const ThemeContext = createContext(null);
 
 const ThemeContextProvider = ({ children }) => {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState(() => {
+    return localStorage.getItem("themeMode") || "light";
+  });
+
   const currentTheme = theme(mode);
 
   const toggleTheme = () => {
-    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+    setMode((prevMode) => {
+      const newMode = prevMode === "light" ? "dark" : "light";
+      localStorage.setItem("themeMode", newMode); // حفظ الوضع الجديد
+      return newMode;
+    });
   };
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem("themeMode");
+    if (savedMode && savedMode !== mode) {
+      setMode(savedMode);
+    }
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ mode, toggleTheme }}>
